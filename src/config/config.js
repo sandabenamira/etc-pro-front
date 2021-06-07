63d4df3e80f64577f7cde32da25a4e2cce7067eb
ibtihelMejri <ibtihel.mejri@adn-expertise.fr>
2021-06-07 15:47:08 +0100

    update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,23 +10,6 @@ module.exports = {
   ? 'https://back.educap.io/api'
   : 'http://localhost:3000/api',
 
-  roleIdSuperAdmin: 1,
-  roleIdAdmin: 2,
-  roleIdProfessor: 3,
-  roleIdParent: 4,
-  roleIdStudent: 5,
-  roleIdSupervisor: 6,
-  roleIdDirector: 7,
-  frequencyMonthly: 4,
-  frequencyQuarterly: 5,
-  frequencyAnnually: 7,
-  unpaidInvoice: 0,
-  partiallyPaidInvoice: 1,
-  billPaid: 2,
-  lateInvoice: 3,
-  privateStatus:2,
-  publicStatus:1,
-  allStatus:0
 };
 
  
