6d094ab77382ae357cc823282b316825ff4d13b5
fatma-beji <fatma.beji@adn-expertise.fr>
2021-08-06 16:58:09 +0100

    onlineTraining

--- src/app/routes/Learning/routes/OnlineTraining/OnlineTraining.js
+++ src/app/routes/Learning/routes/OnlineTraining/OnlineTraining.js
@@ -1,34 +1,64 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import OnlineTrainingList from "./OnlineTrainingList";
+
+
+
 
 /* eslint eqeqeq: "off" */
 export class OnlineTraining extends Component {
   constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    /* eslint eqeqeq: "off" */
-    return (
-      <div className="app-wrapper"
-        
-        style={{
-          marginLeft: '5%',
-          marginRight: '10%',
-        }}
-      >
-        <div className="d-flex flex-column">
-          <div className="p-2" style={{ color: "#4C25B7", fontSize: "26px" }}>
-            E-Learning  -  Formations programmées en ligne 
-          </div>
-        
-      </div>
-      </div>
-    );
-  }
+  super(props);
+  this.state = {};
 }
+render() {
+/* eslint eqeqeq: "off" */
+return (
+  <div className="app-wrapper">
+    <div className="d-flex flex-column">
+      <div className="p-2" style={{ color: "#4C25B7", fontSize: "26px", marginLeft:"15px" }}>
+        E-Learning  -  Formations programmées en ligne 
+      </div>
+      <div className="container-fluid text-center text-md-left">
+      <div className="d-flex flex-sm-wrap justify-content-around justify-content-center">
+      <div className="row">
+      <div className="col-lg-12 col-md-6 col-sm-3 d-flex justify-content-center flex-row " style={{}}>
 
+        <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column m-2 justify-content-center align-items-center" style={{ backgroundColor: "#3F51B5", borderRadius: 15, height: "150px", }}>
+          <h1 style={{ color: "white"}}>Formations en ligne</h1>
+          <h1 style={{ color: "white", fontWeight: "bold" }}>150</h1>
+        </div>
+
+        <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center m-2" style={{ backgroundColor: "#3BBDD5", borderRadius: 15 }}>
+          <h1 style={{ color: "white"}}>Formations en ligne <br /> ce mois</h1>
+          <h1 style={{ color: "white", fontWeight: "bold" }}>25</h1>
+        </div>
+
+        <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center m-2" style={{ backgroundColor: "#F9972D", borderRadius: 15 }}>
+          <h1 style={{ color: "white"}}>Nb d'inscriptions</h1>
+          <h1 style={{ color: "white", fontWeight: "bold" }}>7</h1>
+        </div>
+
+        <div className="col-lg-3 col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center m-2" style={{ backgroundColor: "#F15381", borderRadius: 15 }}>
+          <h1 style={{ color: "white"}}>Nb de personnes <br />  inscrites</h1>
+          <h1 style={{ color: "white", fontWeight: "bold" }}>45</h1>
+        </div>
+      </div>
+      </div>
+      </div>
+      </div>
+    </div>
+    <div className=" d-flex justify-content-end align-items-center flex-row mt-4">
+      <div className="p-2">
+        <OnlineTrainingList openAddTraining={this.openAddTraining} />
+      </div>
+    </div>
+
+  </div>
+
+);
+}
+}
 const mapStateToProps = state => {
   return {};
 };
--- src/app/routes/Learning/routes/OnlineTraining/OnlineTrainingList.js
+++ src/app/routes/Learning/routes/OnlineTraining/OnlineTrainingList.js
@@ -1,34 +1,57 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-// import RemoveSharpIcon from "@material-ui/icons/RemoveSharp";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
-import { orange } from "@material-ui/core/colors";
-import CatalogItem from "./CatalogItem";
+import {  orange } from "@material-ui/core/colors";
+import OnlineTrainingItem from "./OnlineTrainingItem";
 import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from '@material-ui/core/styles';
+//import AddOnlineTraining from "./AddOnlineTraining";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '200%',
+    width: '90%',
+    fontSize:'20px',
+    
+    
+  },
+}));
 
-export default class CatalogList extends Component {
+export default class OnlineTrainingList extends Component {
+   
   render() {
+    const classes = useStyles;
     return (
+
       <div className="d-flex flex-column">
+        
         <div className="d-flex flex-row flex-wrap">
           <div className="p-2 col-md-2 col-sm-1 col-lg-1">
+
             <TextField
               className="textfield"
+              
               id="level_id"
-              //   onChange={this.props.handleChangeLevel(
-              //     "level_id"
-              //   )}
+          
               select
               label="Filtrer par"
-              //   value={this.props.values.level_id}
+              
               SelectProps={{}}
               margin="normal"
               fullWidth
-              variant="outlined"
+              variant="filled"
+              color="primary"
               size="small"
+              
             >
               {[
                 { id: 1, name: "plus récent" },
@@ -47,10 +70,7 @@ export default class CatalogList extends Component {
               name="search"
               label="Rechercher ..."
               size="small"
-              //   onChange={this.props.handleChangeSearchTicket(
-              //     "nameForHistory"
-              //   )}
-              //   value={nameForHistory}
+             
               margin="normal"
               fullWidth
               variant="outlined"
@@ -69,13 +89,9 @@ export default class CatalogList extends Component {
                 size="small"
                 // color="primary"
                 aria-label="Add"
-                onClick={this.props.openAddTraining}
+                //onClick={this.props.o}
               >
-                {/* {this.props.values.open ? (
-                          <RemoveSharpIcon />
-                        ) : (
-                          <AddIcon />
-                        )} */}
+                {}
                 <AddIcon style={{ color: orange[500] }} />
               </Fab>
               &nbsp;&nbsp;&nbsp;
@@ -86,9 +102,9 @@ export default class CatalogList extends Component {
           </div>
         </div>
         <div className="p-2 price-tables row pt-default d-flex justify-content-start ">
-          {[1, 2, 3, 4, 5].map((element, index) => (
+          {[1, 2, 3, 4].map((element, index) => (
             <div className="col-md-6 col-lg-3 col-sm-6 " key={index}>
-              <CatalogItem key={index} />
+              <OnlineTrainingItem key={index} />
             </div>
           ))}
         </div>
