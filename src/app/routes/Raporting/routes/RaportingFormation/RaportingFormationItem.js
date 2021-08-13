511a8e8114930aeadbba5ad7762a0424e6488da3
ouni_mohamed <mohamed.ouni@adn-expertise.fr>
2021-08-13 10:11:45 +0100

    fixed reviews

--- src/app/routes/Raporting/routes/RaportingFormation/RaportingFormationItem.js
+++ src/app/routes/Raporting/routes/RaportingFormation/RaportingFormationItem.js
@@ -6,27 +6,32 @@ import moment from "moment";
 
 
 
-const RaportingFormationItem =({nom,formateur,participants,frais,charges,revenu,rentabilite}) =>
-        <tr style={{backgroundColor :"#DCDCDC"}}>
-            <th scope="row" 
-            style = {{borderTopLeftRadius: "15px", borderBottomLeftRadius: "15px"
-            ,display:"flex", flexDirection: "row", textAlign: "center"}}>
-                <Avatar
-                    className={"size-40 mr-3"}
-                    style={{ fontSize: 11, background: "#00b4d8" }}>
-                        {moment().format("DD")}
-                        <br />
-                        {moment().format("MMM")}
-                    </Avatar>
-                {nom}
-            </th>
-            <td style={{textAlign: "center"}}>{formateur}</td>
-            <td style={{textAlign: "center"}}>{participants}</td>
-            <td style={{textAlign: "center"}}>{frais}</td>
-            <td style={{textAlign: "center"}}>{charges}</td>
-            <td style={{textAlign: "center"}}>{revenu}</td>
-            <td style = {{borderTopRightRadius: "10px", borderBottomRightRadius: "10px",
-             textAlign:"center"}}>{rentabilite}</td>
-        </tr>
+const RaportingFormationItem = ({ nom, formateur, participants, frais, charges, revenu, rentabilite }) =>
+    <tr style={{ backgroundColor: "#f6f6f6" }}>
+        <th scope="row"
+            style={{
+                borderTopLeftRadius: "15px", borderBottomLeftRadius: "15px"
+                , display: "flex", flexDirection: "row", textAlign: "center"
+                , borderTop: "1px solid #f6f6f6",
+            }}>
+            <Avatar
+                className={"size-40 mr-3"}
+                style={{ fontSize: 11,   background: "#00b4d8" }}>
+                {moment().format("DD")}
+                <br />
+                {moment().format("MMM")}
+            </Avatar>
+            {nom}
+        </th>
+        <td style={{ textAlign: "center", borderTop: "1px solid #f6f6f6", }}>{formateur}</td>
+        <td style={{ textAlign: "center", borderTop: "1px solid #f6f6f6", }}>{participants}</td>
+        <td style={{ textAlign: "center", borderTop: "1px solid #f6f6f6", }}>{frais}</td>
+        <td style={{ textAlign: "center", borderTop: "1px solid #f6f6f6", }}>{charges}</td>
+        <td style={{ textAlign: "center", borderTop: "1px solid #f6f6f6", }}>{revenu}</td>
+        <td style={{
+            borderTopRightRadius: "15px", borderBottomRightRadius: "15px",
+            textAlign: "center", borderTop: "1px solid #f6f6f6",
+        }}>{rentabilite}</td>
+    </tr>
 
 export default RaportingFormationItem
