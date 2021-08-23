0574519fc2e1b7351284bbd839a9380797018bc5
fatma-beji <fatma.beji@adn-expertise.fr>
2021-08-23 18:17:20 +0100

    resolve conlict

--- src/app/routes/Administration/routes/AgenceSetting/AgenceSetting.js
+++ src/app/routes/Administration/routes/AgenceSetting/AgenceSetting.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import {connect} from 'react-redux';
-class AgenceSetting extends React.Component {
+import { connect } from 'react-redux';
+import AgenceList from './AgenceList';
+import AddAgence from './AddAgence';
+
+import IntlMessages from "../../../../../util/IntlMessages";
+
+export class AgenceSetting extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false,
+      isOpen: false,
+      isOpenMaterial: false,
     };
+    this.openAddAgence = this.openAddAgence.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+
+  }
+
+  openAddAgence() {
+    this.setState({ isOpen: true });
+  }
+
+  handleCancel() {
+    this.setState({
+      isOpen: false,
+      isOpenMaterial: false,
+    });
   }
 
   render() {
-    /* eslint eqeqeq: "off" */
     return (
       <div
         className="app-wrapper"
         style={{
-          marginLeft: '5%',
-          marginRight: '10%',
+
         }}
       >
-        <h2>module Agence</h2>
+        <div className="p-2" style={{ color: "#4C25B7", fontSize: "26px", marginBottom: "1.5rem" }}>
+          <IntlMessages id="agency.management" />
+        </div>
+        <div className="p-2">
+          <AgenceList openAddAgence={this.openAddAgence} />
+        </div>
+        {this.state.isOpen && (
+          <AddAgence
+            values={this.state}
+            handleCancel={this.handleCancel}
+          />
+        )}
       </div>
     );
   }
